7f31f817f9143f5abf608bcacd886e90be10cbee
Alejandro Morales <84475568+RAMorales-01@users.noreply.github.com>
2024-01-09 08:52:17 -0600

    added the protection equipment base logic

--- GameFunctions.cs/Shopping.cs
+++ GameFunctions.cs/Shopping.cs
@@ -6,14 +6,27 @@ using RankLevels;
 
 namespace GameFunctions
 {
+    #region Global enmus
     public enum ItemCategory
     {
         Weapons = 1,
         Protection = 2,
         Healing = 3
     }
+    public enum ProtectionCategory
+    {
+        Shield = 1,
+        Armor = 2
+    }
+    #endregion
+
+    #region Shop class
     internal class EquipmentItemStore
     {
+        /// <summary>
+        /// Input function for category selection of the store. 
+        /// </summary>
+        /// <param name="player"></param>
         internal static void Shop(PlayerChar player)
         {
             Console.Clear();
@@ -32,8 +45,16 @@ namespace GameFunctions
                 Shopping.HandleShoppingChoice(player, selectedCategory);
             }
         }
+
+        #region Selection 
         internal class Shopping
         {
+            /// <summary>
+            /// Handles each method for the different equipment the player can buy in the store
+            /// after the player has selected the category in the previous Shop method.
+            /// </summary>
+            /// <param name="player"></param>
+            /// <param name="selectedCategory"></param>
             internal static void HandleShoppingChoice(PlayerChar player, ItemCategory selectedCategory)
             {
                 switch(selectedCategory)
@@ -43,7 +64,7 @@ namespace GameFunctions
                         break;
 
                     case ItemCategory.Protection:
-                        BuyProtection(player);
+                        SelectProtection(player);
                         break;
 
                     case ItemCategory.Healing:
@@ -55,10 +76,60 @@ namespace GameFunctions
                         break;
                 }
             }
+
+            /// <summary>
+            /// Input function for protection category selection of the store. 
+            /// </summary>
+            /// <param name="player"></param>
+            internal static void SelectProtection(PlayerChar player)
+            {
+                Console.Clear();
+                Console.WriteLine("What are you buying?\n");
+                Console.WriteLine("\n1- Shield\n2- Armor");
+                string? input = Console.ReadLine();
+
+                if(!int.TryParse(input, out int choice))
+                {
+                    Console.WriteLine("Input not valid, you must choose only the presented options");
+                }
+                else
+                {
+                    ProtectionCategory selectedCategory = (ProtectionCategory)choice;
+                    Shopping.HandleProtectionChoice(player, selectedCategory);
+                }
+            }
+
+            /// <summary>
+            /// Handles the 2 methods for the choices of protection equipment the player can buy in the store.
+            /// </summary>
+            /// <param name="player"></param>
+            /// <param name="selectedCategory"></param>
+            private static void HandleProtectionChoice(PlayerChar player, ProtectionCategory selectedCategory)
+            {
+                switch(selectedCategory)
+                {
+                    case ProtectionCategory.Shield:
+                        BuyShield(player);
+                        break;
+
+                    case ProtectionCategory.Armor:
+                        BuyArmor(player);
+                        break;
+                }
+            }
+            #endregion
+            
+            #region Buy equipment methods
+            /// <summary>
+            /// Handles the different Sword objects the player can buy and use in the game, takes the player
+            /// instance as parameter and checks the player hero rank to match wich sword the player can currently 
+            /// access, for example if the player hero rank is "A" then he/she can access to the Sword rankA instance.
+            /// </summary>
+            /// <param name="player"></param>
             internal static void BuySword(PlayerChar player)
             {
-                bool buyEquipment;
-                int costEquipment = 0;
+                bool buyEquipment; // Stores the return from the BuyConfirmation method
+                int costEquipment = 0; // Stores the equipment price, if BuyConfirmation returns true subtracts cost from player current gold
 
                 Console.Clear();
                 Console.WriteLine("With your current Rank you are able to buy this weapon:\n");
@@ -79,29 +150,67 @@ namespace GameFunctions
                     
                     case Rank.B:
                         Sword rankB = new Sword(2);
+                        costEquipment = rankB.EquipmentPrice;
                         SwordInfo(rankB);
+                        buyEquipment = BuyConfirmation(player, costEquipment);
+                        if(buyEquipment == true)
+                        {
+                            player.EquipSword(rankB);
+                            player.Gold -= costEquipment;
+                        }
                         break;
                     
                     case Rank.A:
                         Sword rankA = new Sword(3);
+                        costEquipment = rankA.EquipmentPrice;
                         SwordInfo(rankA);
+                        buyEquipment = BuyConfirmation(player, costEquipment);
+                        if(buyEquipment == true)
+                        {
+                            player.EquipSword(rankA);
+                            player.Gold -= costEquipment;
+                        }
                         break;
                     
                     case Rank.S:
                         Sword rankS = new Sword(4);
+                        costEquipment = rankS.EquipmentPrice;
                         SwordInfo(rankS);
+                        buyEquipment = BuyConfirmation(player, costEquipment);
+                        if(buyEquipment == true)
+                        {
+                            player.EquipSword(rankS);
+                            player.Gold -= costEquipment;
+                        }
                         break;
                     
                     case Rank.SS:
                         Sword rankSS = new Sword(5);
+                        costEquipment = rankSS.EquipmentPrice;
                         SwordInfo(rankSS);
+                        buyEquipment = BuyConfirmation(player, costEquipment);
+                        if(buyEquipment == true)
+                        {
+                            player.EquipSword(rankSS);
+                            player.Gold -= costEquipment;
+                        }
                         break;
                 }
             }
-            internal static void BuyProtection(PlayerChar player)
+
+            internal static void BuyShield(PlayerChar player)
             {
-                
+            
             }
+            internal static void BuyArmor(PlayerChar player)
+            {
+
+            }
+
+            /// <summary>
+            /// Handles the different Healing potion objects the player can buy and use in the game, doesn't need
+            /// any check and the player can access to all the type of potions at any time.
+            /// </summary>
             internal static void BuyHealing()
             {
                 Console.WriteLine("These are 3 types of healing potions currently availables:\n");
@@ -137,6 +246,13 @@ namespace GameFunctions
                     }
                 }
             }
+            #endregion
+
+            #region Equipment information display methods
+            /// <summary>
+            /// Displays information of the current Sword object the player can access during the BuySword method. 
+            /// </summary>
+            /// <param name="currentRankSword"></param>
             private static void SwordInfo(Sword currentRankSword)
             {
                 Console.WriteLine($"\t\t\t{currentRankSword.Name}\n");
@@ -145,12 +261,50 @@ namespace GameFunctions
                 Console.WriteLine($"Weight: {currentRankSword.EquipmentWeight} kgs");
                 Console.WriteLine($"Price: {currentRankSword.EquipmentPrice} gold");
             }
+
+            /// <summary>
+            /// Displays information of the current Shield object the player can access during the BuyShield method.
+            /// </summary>
+            /// <param name="currentRankShield"></param>
+            private static void ShieldInfo(Shield currentRankShield)
+            {
+                Console.WriteLine($"\t\t\t{currentRankShield.Name}\n");
+                Console.WriteLine($"\"{currentRankShield.EquipmentInfo}\"");
+                Console.WriteLine($"\nDefense: {currentRankShield.Defense}");
+                Console.WriteLine($"Weight: {currentRankShield.EquipmentWeight} kgs");
+                Console.WriteLine($"Price: {currentRankShield.EquipmentPrice} gold");
+            }
+
+            /// <summary>
+            /// Displays information of the current Armor object the player can access during the BuyArmor method.
+            /// </summary>
+            /// <param name="currentRankShield"></param>
+            private static void ArmorInfo(Shield currentRankArmor)
+            {
+                Console.WriteLine($"\t\t\t{currentRankArmor.Name}\n");
+                Console.WriteLine($"\"{currentRankArmor.EquipmentInfo}\"");
+                Console.WriteLine($"\nDefense: {currentRankArmor.Defense}");
+                Console.WriteLine($"Weight: {currentRankArmor.EquipmentWeight} kgs");
+                Console.WriteLine($"Price: {currentRankArmor.EquipmentPrice} gold");
+            }
+
+            /// <summary>
+            /// Displays information of the Healing potions the player can access in the BuyHealing method. 
+            /// </summary>
+            /// <param name="potion"></param>
             private static void PotionInfo(HealingItems potion)
             {
                 Console.WriteLine($"Info: {potion.ItemDescription}");
                 Console.WriteLine($"Price: {potion.ItemPrice} gold");
             }
+            #endregion
             
+            /// <summary>
+            /// Handles the confirmation checks for buying equipment in the store.
+            /// </summary>
+            /// <param name="player"></param>
+            /// <param name="costEquipment"></param>
+            /// <returns>boolean</returns>
             private static bool BuyConfirmation(PlayerChar player, int costEquipment)
             {
                 string? input = string.Empty;
@@ -181,7 +335,7 @@ namespace GameFunctions
                 }
                 else if(input == "Y" && player.Gold < costEquipment)
                 {
-                    Console.WriteLine("Sorry you don't have enough gold");
+                    Console.WriteLine("\nSorry you don't have enough gold\n");
                     return false;
                 }
                 else
@@ -191,4 +345,5 @@ namespace GameFunctions
             } 
         }
     }
+    #endregion
 }
